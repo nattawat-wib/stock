6e4e8b0bac608fb0c89cc0bc249824142a6da40c
nattawat-wib <nattawat.wib@gmail.com>
2024-07-28 20:33:31 +0700

    refactor: clearn unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,16 @@
-"use client";
+"use client"
 
 import { NextUIProvider } from "@nextui-org/react";
 import { Input, Switch } from "@nextui-org/react";
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun, faBell } from "@fortawesome/free-regular-svg-icons";
 import { Button } from './components/button'
 
-import { useForm, SubmitHandler } from "react-hook-form";
-
 export default function Home() {
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    const {register, handleSubmit, watch} = useForm()
-
     return (
         <NextUIProvider>
-            <main className={`min-h-screen p-24 ${isDarkMode ? "dark" : ""} text-foreground bg-background`}>
+            <main className={`min-h-screen p-24 ${true ? "dark" : ""} text-foreground bg-background`}>
                 <Switch
-                    onChange={() => setIsDarkMode((prev) => !prev)}
                     startContent={<FontAwesomeIcon icon={faMoon} />}
                     endContent={<FontAwesomeIcon icon={faSun} />}
                 />
